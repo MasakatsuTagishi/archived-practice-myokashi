144f66fae99ecf751ba35c0b83677f44e91056d9
MasakatsuTagishi <m.t-10.22@docomo.ne.jp>
2021-06-10 13:27:04 +0900

    Initial Commit

---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by 田岸将勝 on 2021/06/10.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
